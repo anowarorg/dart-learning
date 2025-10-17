e845a2a81fc2d858ea0392aabab7084aff9d9125
anowarorg <103601765+anowarorg@users.noreply.github.com>
2025-10-17 12:17:35 +0600

    First Programming

---
+++ first_program.dart
@@ -0,0 +1,13 @@
+void main() {
+  print("Hello World"); // First Comment
+
+  print("3+5"); // String print
+
+  print('''
+This is
+a Multiline
+Quotation Print
+''');
+
+  print("This is the Sum of 3+5 = ${3 + 5}");
+}
