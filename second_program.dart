267a34ad2167d68492734187bdeab0a2ca7938da
anowarorg <103601765+anowarorg@users.noreply.github.com>
2025-10-18 12:12:43 +0600

    2nd dart practice

---
+++ second_program.dart
@@ -0,0 +1,14 @@
+void main() {
+  String name = "Anowar Hossain";
+  int age = 24;
+  bool isstudent = true;
+
+  List<String> sections = ['One', 'Two', 'Three'];
+  Map<String, int> studentInfo = {'ID': 01, "Roll": 3870};
+
+  print('My Name is $name');
+  print('My Age is $age');
+  print('If I am a Student? $isstudent');
+  print("The List of The Sections $sections");
+  print("My Student Information is $studentInfo");
+}
