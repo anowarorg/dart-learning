5d2f87bb65ee44bcaf679d161b6202c99ef44580
anowarorg <103601765+anowarorg@users.noreply.github.com>
2025-10-19 10:57:21 +0600

    3rd Dart Program

---
+++ int_double_num.dart
@@ -0,0 +1,16 @@
+void main() {
+  int id;
+  double cgpa;
+
+  id = 1;
+  cgpa = 3.60;
+
+  num unknown_value = 10;
+
+  print(unknown_value is int);
+  print(id);
+  print(cgpa);
+
+  num name = 10.5;
+  print(name is int);
+}
